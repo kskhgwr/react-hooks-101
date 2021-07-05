fd300db7bd7578b9437239aaa1617dbe3fe0f97d
kskhgwr <kskhgwr@gmail.com>
2021-07-05 13:54:21 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,7 +13,7 @@ const events = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body }
-      cosnt length = state.length;
+      const length = state.length;
       const id = length === 0 ?  1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
     case 'DELETE_EVENT':
